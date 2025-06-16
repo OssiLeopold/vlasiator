06327981e08d0b1d90094fcf4188b471daba8dd1
rxelmer <elme@live.fi>
2025-06-16 16:49:51 +0300

    Removed k components from u and B

--- projects/Turbulence/Turbulence.cpp
+++ projects/Turbulence/Turbulence.cpp
@@ -183,8 +183,8 @@ Realf Turbulence::fillPhaseSpace(spatial_cell::SpatialCell *cell,
          //Real uperp = amplitude.at(idx) * cos(kwave_x * x + kwave_y * y + phase.at(idx));
          //Real upara = 0;
          
-         ux += - amplitude.at(idx) * kwave_y * cos(kwave_x * x + kwave_y * y + phase.at(idx));
-         uy += amplitude.at(idx) * kwave_x * sin(kwave_x * x + kwave_y * y + phase.at(idx));
+         ux += - amplitude.at(idx) * cos(kwave_x * x + kwave_y * y + phase.at(idx));
+         uy += amplitude.at(idx) * sin(kwave_x * x + kwave_y * y + phase.at(idx));
          uz += 0;
 
          //Real xpar = x * cosalpha + y * sinalpha;
@@ -280,8 +280,8 @@ void Turbulence::setProjectBField(FsGrid<std::array<Real, fsgrids::bfield::N_BFI
                    //Real Bperp = B1 * cos(kwave_x * x[0] + kwave_y * x[1] + phase.at(idx));
                    //Real Bpara = 0;
 
-                   Bx += -B1 * kwave_y * cos(kwave_x * x[0] + kwave_y * x[1] + phase.at(idx));
-                   By += B1 * kwave_x * sin(kwave_x * x[0] + kwave_y * x[1] + phase.at(idx));
+                   Bx += -B1 * cos(kwave_x * x[0] + kwave_y * x[1] + phase.at(idx));
+                   By += B1 * sin(kwave_x * x[0] + kwave_y * x[1] + phase.at(idx));
                    Bz += 0;
 
                   /*  Real Bperp = B1 * sin(kwave * xpar + phase.at(idx));
