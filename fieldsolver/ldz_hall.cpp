e63cad5073ce4854846911954b4454293aac9bf1
OssiLeopold <elme@live.fi>
2026-08-19 14:18:44 +0300

    Change EHallGrid to ehall in other place as well

--- fieldsolver/ldz_hall.cpp
+++ fieldsolver/ldz_hall.cpp
@@ -1051,7 +1051,7 @@ void calculateEdgeHallTermComponents(fsgrids::perbspan perbs,
          ehall[fsgrids::ehall::EXHALL_011_111] = val;
       });
 
-      fsgrid.updateGhostCells(EHallGrid);
+      fsgrid.updateGhostCells(ehall);
 
       ehall[fsgrids::ehall::EXHALL_000_100] = EXHall;
       ehall[fsgrids::ehall::EXHALL_010_110] = EXHall;
